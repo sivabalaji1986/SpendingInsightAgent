14e41db45030bbeee3748a86d219c613f82ba53e
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2025-11-20 17:29:19 +0800

    Added Application and Application Test

---
+++ src/main/java/com/hbs/spending_insight_agent/SpendingInsightAgentApplication.java
@@ -0,0 +1,13 @@
+package com.hbs.spending_insight_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendingInsightAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendingInsightAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hbs/spending_insight_agent/SpendingInsightAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hbs.spending_insight_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendingInsightAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
