2d67bdd92fe426b5a8bfd75074cc03baa01643b7
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2025-11-29 01:00:24 +0800

    Updated TransactionRepository with right generic type

--- src/main/java/com/hbs/spending_insight_agent/repository/TransactionRepository.java
+++ src/main/java/com/hbs/spending_insight_agent/repository/TransactionRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDate;
 import java.util.List;
 
-public interface TransactionRepository extends JpaRepository<Transaction, String> {
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
     List<Transaction> findByAccountIdAndDateBetween(String accountId, LocalDate from, LocalDate to);
 }
